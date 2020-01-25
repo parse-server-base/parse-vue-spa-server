163511e76b87890da08ae60451a16c9687738614
wahengchang <wahengchang@gmail.com>
2020-01-25 16:25:00 +0800

    disable no-consol

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
   ],
   rules: {
     // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-console': false,
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
